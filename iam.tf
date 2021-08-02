289d14fefd2786725fff1823732b1a33fb2ee7d2
bdalli <bdalli52@gmail.com>
2021-08-02 13:21:51 +0100

    added policy doc for users

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
-# policy for buckets 
+# lambda_policy for buckets 
 
 data "aws_iam_policy_document" "s3_lambda_policy" {
   statement {
@@ -33,4 +33,43 @@ data "aws_iam_policy_document" "lambda_assume_role" {
   }
 }
 
+
+# user policy document for bucket access  
+
+data "aws_iam_policy_document" "user_a_s3_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [module.s3_bucket["jpeg-bucket-a"].s3_bucket_arn]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject"
+    ]
+    resources = ["${module.s3_bucket["jpeg-bucket-a"].s3_bucket_arn}/*"]
+  }
+}
+data "aws_iam_policy_document" "user_b_s3_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [module.s3_bucket["jpeg-bucket-b"].s3_bucket_arn]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+
+      "s3:GetObject"
+
+    ]
+    resources = ["${module.s3_bucket["jpeg-bucket-b"].s3_bucket_arn}/*"]
+  }
+}
 
